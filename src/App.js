f557fe924b86ad09755afc02f3aa035e2d6520d8
jbmakib <mdfoujolmobin@gmail.com>
2021-10-04 21:36:01 +0600

    About updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer/Footer";
 import NotFound from "./components/NotFound/NotFound";
 import Home from "./components/Home/Home";
 import Services from "./components/Services/Services";
+import About from "./components/About/About";
 
 function App() {
     return (
@@ -20,6 +21,9 @@ function App() {
                     </Route>
                     <Route path="/services">
                         <Services></Services>
+                    </Route>
+                    <Route path="/about">
+                        <About></About>
                     </Route>
                     <Route path="*">
                         <NotFound></NotFound>
---
+++ src/components/About/About.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+const About = () => {
+    return (
+        <Container>
+            <Row xs={1} md={2} className="align-items-center my-4">
+                <Col>
+                    <div>
+                        <img
+                            src="https://shelly.merku.love/wp-content/uploads/2020/11/abt2.png"
+                            alt="About banner"
+                            className="img-fluid"
+                        />
+                    </div>
+                </Col>
+                <Col>
+                    <h3 className="homeFront-h1">
+                        Our
+                        <span className="text-orange"> Mission</span>
+                    </h3>
+                    <p className="h5 border-start border-4 ps-4 border-orange fw-normal">
+                        where you hope to see it in the future. The mission
+                        provides an overview of the steps planned to achieve
+                        that future. A vision is concise and easy to recall,
+                        whereas a mission is lengthier and more explanatory in
+                        nature.
+                    </p>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default About;
