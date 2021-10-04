13f444a8f4cfc41a8f9759b89366a3a7f3f6836f
jbmakib <mdfoujolmobin@gmail.com>
2021-10-04 08:50:12 +0600

    Nav Created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <NavLink to="/" className="text-decoration-none h4">
+                    React-Bootstrap
+                </NavLink>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <NavLink
+                            to="/home"
+                            className="text-decoration-none text-secondary m-2"
+                            activeClassName="text-dark"
+                        >
+                            Home
+                        </NavLink>
+                        <NavLink
+                            to="/about"
+                            className="text-decoration-none text-secondary m-2"
+                            activeClassName="text-dark"
+                        >
+                            About Us
+                        </NavLink>
+                        <NavLink
+                            to="/services"
+                            className="text-decoration-none text-secondary m-2"
+                            activeClassName="text-dark"
+                        >
+                            Services
+                        </NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
